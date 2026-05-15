b870e0dccdb8e66ab1c051caacd2f1e1fdbb1304
Dima-shima <shifrin1970@gmail.com>
2026-05-15 19:16:58 +0300

    implemented every-second

--- app/main.py
+++ app/main.py
@@ -3,17 +3,15 @@ from time import sleep
 
 
 def main() -> None:
-    try:
-        while True:
-            now = datetime.now()
-            result = now.strftime("%Y-%m-%d %H:%M:%S")
-            hours = now.strftime("%H")
-            minutes = now.strftime("%M")
-            seconds = now.strftime("%S")
-            file_name = f"app-{hours}_{minutes}_{seconds}.log"
-            with open(file_name, "w") as f:
-                f.write(result)
-            print(now, file_name)
-            sleep(1)
-    except:
-        raise KeyboardInterrupt
+    while True:
+        now = datetime.now()
+        result = now.strftime("%Y-%m-%d %H:%M:%S")
+        hours = now.strftime("%H")
+        minutes = now.strftime("%M")
+        seconds = now.strftime("%S")
+        file_name = f"app-{hours}_{minutes}_{seconds}.log"
+        with open(file_name, "w") as f:
+            f.write(result)
+        print(now, file_name)
+        sleep(1)
+    raise KeyboardInterrupt
